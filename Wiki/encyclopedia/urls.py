27396a749074fa6f6b3de4ce01e7c42a29730e99
Kuro <quangacon02@gmail.com>
2023-03-12 16:11:44 +0700

    Implement search

--- Wiki/encyclopedia/urls.py
+++ Wiki/encyclopedia/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
-    path("search/", views.search, name="index"),
-    path("<str:title>/", views.title, name="entry")
+    path("", lambda req: views.redirect('wiki/')),
+    path("wiki/", views.index, name="index"),
+    path("wiki/search/", views.search, name="search"),
+    path("wiki/<str:title>/", views.title, name="entry")
 ]
--- Wiki/encyclopedia/views.py
+++ Wiki/encyclopedia/views.py
@@ -1,33 +1,50 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import Http404
+from django.urls import reverse
 from markdown import markdown
 from django import forms
 
 from . import util
 
 
-class Aform(forms.Form):
-    input = forms.CharField(label="input", max_length=100)
-
-
 def index(request):
-    return render(request, "encyclopedia/index.html", {
-        "entries": util.list_entries()
-    })
+    return render(request, "encyclopedia/index.html",
+                  {"entries": util.list_entries()})
+
 
 def title(request, title):
-        entry = util.get_entry(title)
-        if entry is None:
-            raise Http404 
+    # Get matched entry
+    entry = util.get_entry(title)
 
-        return render(request, "encyclopedia/entry.html", {
-            "title": title,
-            "entry": markdown(util.get_entry(title))
-        })
+    # if none, raise 404
+    if entry is None:
+        raise Http404
+    # else return that entry
+    return render(request, "encyclopedia/entry.html", {
+        "title": title,
+        "entry": markdown(util.get_entry(title))
+    })
+
 
 def search(request):
-     form = Aform(request.GET)
+    # Take user's search
+    query = request.GET["q"].lower()
 
-     if form.is_valid():
-          query = form.cleaned_data("q")
-          
+    # If matched with any entry, return that entry
+    if util.get_entry(query):
+        return redirect(reverse("entry", kwargs={"title": query}))
+    else:
+        # Initialize material for search
+        entries = util.list_entries()
+        matches = []
+
+        # For every entry, check if it contain user search disregard case
+        for entry in entries:
+            if query in entry.lower():
+                matches += [entry]
+
+        # Return matched entry 
+        return render(request, "encyclopedia/search.html", {
+            "title": query,
+            "matches": matches
+        })
