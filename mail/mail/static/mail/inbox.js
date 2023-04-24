87a0e2488f371a10e13f7cf22d6985a0b81238b5
Kuro <quangacon02@gmail.com>
2023-04-24 12:54:04 +0700

    Revert "Implement compose mail"
    This reverts commit 80aa97709fdffbd65ecb675119d45df8fb0e15bc.

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -1,11 +1,10 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
   // Use buttons to toggle between views
   document.querySelector('#inbox').addEventListener('click', () => load_mailbox('inbox'));
   document.querySelector('#sent').addEventListener('click', () => load_mailbox('sent'));
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
-  document.querySelector("#compose-form").addEventListener("submit", sendEmail)
 
   // By default, load the inbox
   load_mailbox('inbox');
@@ -26,7 +25,7 @@ function compose_email() {
 
 
 function load_mailbox(mailbox) {
-
+  
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
