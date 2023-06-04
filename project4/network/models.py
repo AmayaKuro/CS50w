44dff9c2eaaa51ec0c8bea1ff50405d401d1ed6b
Kuro <quangacon02@gmail.com>
2023-06-04 23:00:17 +0700

    Implement main page (infinite scroll)

--- project4/network/models.py
+++ project4/network/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+import datetime
 
 
 class User(AbstractUser):
@@ -10,6 +11,15 @@ class Post(models.Model):
     owner = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
     likes = models.IntegerField(default=0)
     timeStamp = models.DateTimeField(auto_now_add=True)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "content": self.content,
+            "owner": self.owner.username,
+            "likes": self.likes,
+            "timeStamp": self.timeStamp.timestamp(),
+        }
 
 class Comment(models.Model):
     comment = models.TextField()
