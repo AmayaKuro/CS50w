99a8ad5c98ed677b0ea614d63b8f41768a49108f
Kuro <quangacon02@gmail.com>
2023-07-06 12:32:52 +0700

    Emergency Push

--- project4/network/models.py
+++ project4/network/models.py
@@ -23,7 +23,7 @@ class User(AbstractUser):
 class Post(models.Model):
     content = models.TextField()
     owner = models.ForeignKey("User", on_delete=models.CASCADE, related_name="posts")
-    likes = models.IntegerField(default=0)
+    likes = models.ManyToManyField("User", related_name="likedPosts", blank=True)
     timeStamp = models.DateTimeField(auto_now_add=True)
 
     def serialize(self):
@@ -31,11 +31,6 @@ class Post(models.Model):
             "id": self.id,
             "content": self.content,
             "owner": self.owner.username,
-            "likes": self.likes,
+            "likes": self.likes.count(),
             "timeStamp": self.timeStamp.timestamp(),
         }
-
-class Comment(models.Model):
-    comment = models.TextField()
-    post = models.ForeignKey("Post", on_delete=models.CASCADE, related_name="comments")
-    commenter = models.ForeignKey("User", on_delete=models.CASCADE, related_name="comments")
