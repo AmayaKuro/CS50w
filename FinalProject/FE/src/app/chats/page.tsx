8402649ec9a6d0b7af42e9b5f757e35da58d4452
Kuro <quangacon02@gmail.com>
2023-10-13 13:54:29 +0700

    Relocate g4f module, Add Create chat to conversations route, Modify Conversation and Response models' structure, Redeclare Session object (useSession)

--- FinalProject/FE/src/app/chats/page.tsx
+++ FinalProject/FE/src/app/chats/page.tsx
@@ -1,8 +1,13 @@
 "use client"
+import { useSession } from "next-auth/react"
+
 import { Button } from "@/components/signOutButton.component"
+import { useState } from "react"
 
 
 export default function Home() {
+    const {data, update} = useSession()
+    data?.access_token
     return (
         <div>
             <h1>Home</h1>
