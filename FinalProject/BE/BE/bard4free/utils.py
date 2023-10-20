5e901226aae5cb0f5bc4471c94f28356516120c2
Kuro <quangacon02@gmail.com>
2023-10-21 01:16:39 +0700

    - Restructure API: >> > Split conversation and response manipulation into  2 route >> - Remove conversation in Serializer's Meta class >> - Make Response saving a reusable function

--- FinalProject/BE/BE/bard4free/utils.py
+++ FinalProject/BE/BE/bard4free/utils.py
@@ -1,8 +1,20 @@
 import re
 
+from .g4f.typing import ChatResponse
+from .serializers import ResponseSerializer
+
+
 def idCheck(*args):
     for arg in args:
-        if not arg or not re.match(r'(c|r|rc)_[a-z0-9]{16}', arg):
+        if not arg or not re.match(r"(c|r|rc)_[a-z0-9]{16}", arg):
             return False
     return True
 
+
+def saveResponse(conversation_key, chatData: ChatResponse):
+    serializer = ResponseSerializer(data=chatData)
+    if serializer.is_valid():
+        # Saved by passing the conversation instance to the save()
+        serializer.save(conversation=conversation_key)
+        return True
+    return False
