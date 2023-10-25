73565a470cb5f5f88e6b8ba275f347f3d3961ca8
Kuro <quangacon02@gmail.com>
2023-10-26 00:46:42 +0700

    Form basic block for chats, update some components for title

--- FinalProject/FE/src/components/navbar/navbar.component.tsx
+++ FinalProject/FE/src/components/navbar/navbar.component.tsx
@@ -1,15 +1,19 @@
 import React, { ReactNode } from 'react';
 
+import styles from '@/css/navbar/navbar.module.css';
+
 interface P {
     children: ReactNode;
 }
 
 const Navbar: React.FC<P> = ({ children }) => {
     return (
-        <nav>
-            <div>Header?</div>
+        <nav id={styles.navbar}>
+            <div id={styles.navHeader}>
+                <img src="/favicon.ico" alt="logo" />
+            </div>
             {children}
-            <div>signout</div>
+            <div id={styles.navFooter}>signout</div>
         </nav>
     );
 };
--- FinalProject/FE/src/components/navbar/title.container.component.tsx
+++ FinalProject/FE/src/components/navbar/title.container.component.tsx
@@ -1,10 +1,15 @@
 "use client";
 import Link from 'next/link';
+import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import { useState, useEffect, useCallback } from 'react';
+import Button from "@mui/material/Button"
 
 import { type ConversationProps, useConversastion } from '@/assets/providers/conversation';
 import { fetchAPI } from '@/assets/fetch/base';
+import { PlusIcon } from "@/assets/mui/icon";
+
+import styles from '@/css/navbar/title.module.css'
 
 
 const TitleContainer: React.FC = () => {
@@ -13,23 +18,36 @@ const TitleContainer: React.FC = () => {
 
     const { data: session } = useSession();
 
+    const router = useRouter();
+
     useEffect(() => {
         if (!session?.access_token || hasFetched) {
             return
         }
 
-        fetchAPI('conversations', session?.access_token).then((res: ConversationProps[]) => {
-            setConversations([["", ""], ...res]);
+        fetchAPI('conversations', {
+            headers: {
+                Authorization: `Bearer ${session.access_token}`,
+            },
+        }).then((res: ConversationProps[]) => {
+            setConversations([...res]);
             setHasFetched(true);
         });
 
     }, [session, hasFetched]);
 
     return (
-        <div>
+        <div className={styles.titleContainer}>
+            <div className={styles.newConvContainer}>
+                <Button 
+                className={styles.newConvBtn} 
+                size="large" startIcon={<PlusIcon />} 
+                onClick={() => router.push("/chats")}>New Chat</Button>
+            </div>
+
             {conversations.map((conversation) => (
                 <div key={conversation.conversation_id}>
-                    <Link href={`chats/${conversation.conversation_id}`}>
+                    <Link href={`/chats/${conversation.conversation_id}`}>
                         {conversation.title}
                     </Link>
                 </div>
