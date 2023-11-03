22bedd6772c209db58bd8d6541e0d0851c8df9d9
Kuro <quangacon02@gmail.com>
2023-11-03 14:11:04 +0700

    Fix naming in Conversation context. Group setting response's code

--- FinalProject/FE/src/components/main/chat.tsx
+++ FinalProject/FE/src/components/main/chat.tsx
@@ -1,46 +1,13 @@
-import React, { useEffect, useState } from "react";
-import { useSession } from "next-auth/react";
 import Markdown from 'markdown-to-jsx';
 
 import AccountCircle from "@mui/icons-material/AccountCircle";
 
-import { BEfetch } from "@/assets/fetch/BEfetch";
+import { gettingResponseProps } from "@/assets/providers/conversation";
 
 import styles from "@/css/main/chat.module.css";
 
 
-type Response = {
-    response_id: string;
-    choice_id: string;
-    message: string;
-    log: string;
-};
-
-
-const Chat: React.FC<{ conversation_id: string }> = ({ conversation_id }) => {
-    const [responses, setResponses] = useState<Response[]>([]);
-    const [hasFetched, setHasFetched] = useState(false);
-    const [loading, setLoading] = useState(false);
-
-    const { data: session } = useSession();
-
-
-    useEffect(() => {
-        if (loading) return;
-        
-        if (session?.access_token && !hasFetched) {
-            setLoading(true);
-            BEfetch(`/response?conversation_id=${conversation_id}`, {
-                headers: {
-                    Authorization: `Bearer ${session.access_token}`
-                },
-            }).then((res: Response[]) => {
-                setResponses(res);
-                setHasFetched(true);
-                setLoading(false);
-            })
-        }
-    }, [session?.access_token, hasFetched, loading]);
+const Chat: React.FC<{ responses: gettingResponseProps[] }> = ({ responses }) => {
 
     return (
         <>
